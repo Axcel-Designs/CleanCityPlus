c8396f88f0c9a6df2f4fcdf378bef49c2c2bfcc6
excel <axceldesigns@gmail.com>
2025-09-08 11:38:58 +0100

    waste

--- app/waste/page.js
+++ app/waste/page.js
@@ -1,61 +1,80 @@
-import { AccordionBody, AccordionHeader, AccordionItem } from "react-bootstrap";
-import Accordion from "react-bootstrap/Accordion";
+"use client";
+
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import { wasteCat } from "@/utils/facts";
 
-export default function wasteCategories() {
+export default function WasteCategories() {
   return (
-    <main className="container mx-auto my-20">
-      <section className="text-center text-gray-800 mb-4 grid gap-2">
-        <p className="text-6xl text-green-700">Waste Categories</p>
-        <em>
-          Learn about different types of waste and how to properly dispose of
-          them to maximize recycling and minimize environmental impact.
-        </em>
-        <p className="text-sm text-yellow-700">
-          Click on a category to learn more about proper disposal and helpful
-          tips.
-        </p>
-      </section>
-      <section className="mb-25">
-      <Accordion flush>
-        <div className="container mx-auto flex flex-col gap-4">
-          {wasteCat.map((waste, i) => (
-            <AccordionItem eventKey={`'${i}'`} key={waste.id}>
-              <AccordionHeader>
-                <div className={`${waste.color} text-xl flex gap-2`}>
-                  <span>{waste.icon}</span>
-                  {waste.name}
-                </div>
-              </AccordionHeader>
-              <AccordionBody className={`${waste.bgColor}`}>
-                <dl>
-                  <dt>Waste Tips:</dt>
-                  {waste.tips.map((tip, i) => (
-                    <dd key={i}>{tip}.</dd>
-                  ))}
-                </dl>
-                <br />
-                <dl>
-                  <dt>Waste disposal guidelines:</dt>
-                </dl>
-                <dd>{waste.disposal}</dd>
-                <dl>
-                  <dt>Examples:</dt>
-                  <dd className="flex flex-wrap gap-x-4 ">
-                    {waste.examples.map((eg, i) => (
-                      <div key={i} className={`${waste.color}`}>
-                        {eg}
+    <main>
+      <Container className="my-5">
+        {/* Section Heading */}
+        <section className="text-center text-gray-700 mb-4">
+          <Row className="gy-2">
+            <Col>
+              <p className="fs-1 fw-bold text-green-700">Waste Categories</p>
+              <em>
+                Learn about different types of waste and how to properly dispose
+                of them to maximize recycling and minimize environmental impact.
+              </em>
+              <p className="text-sm text-yellow-700 mt-2">
+                Click on a category to learn more about proper disposal and
+                helpful tips.
+              </p>
+            </Col>
+          </Row>
+        </section>
+
+        {/* Accordion Section */}
+        <section className="mb-5">
+          <Accordion flush>
+            <Row className="gy-4">
+              {wasteCat.map((waste, i) => (
+                <Col xs={12} key={waste.id}>
+                  <AccordionItem eventKey={i.toString()}>
+                    <AccordionHeader>
+                      <div className={`${waste.color} fs-5 d-flex gap-2`}>
+                        <span>{waste.icon}</span>
+                        {waste.name}
                       </div>
-                    ))}
-                  </dd>
-                </dl>
-              </AccordionBody>
-            </AccordionItem>
-          ))}
-        </div>
-      </Accordion>
-      </section>
-        
+                    </AccordionHeader>
+                    <AccordionBody className={`${waste.bgColor}`}>
+                      <dl>
+                        <dt>Waste Tips:</dt>
+                        {waste.tips.map((tip, idx) => (
+                          <dd key={idx}>{tip}.</dd>
+                        ))}
+                      </dl>
+                      <br />
+                      <dl>
+                        <dt>Waste disposal guidelines:</dt>
+                      </dl>
+                      <dd>{waste.disposal}</dd>
+                      <dl>
+                        <dt>Examples:</dt>
+                        <dd className="d-flex flex-wrap gap-3">
+                          {waste.examples.map((eg, idx) => (
+                            <div key={idx} className={`${waste.color}`}>
+                              {eg}
+                            </div>
+                          ))}
+                        </dd>
+                      </dl>
+                    </AccordionBody>
+                  </AccordionItem>
+                </Col>
+              ))}
+            </Row>
+          </Accordion>
+        </section>
+      </Container>
     </main>
   );
 }
