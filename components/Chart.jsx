f58ac1612932a44908b4548c7da1fe6fa5d2e764
excel <axceldesigns@gmail.com>
2025-09-08 01:50:14 +0100

    tracker

--- components/Chart.jsx
+++ components/Chart.jsx
@@ -1,17 +1,14 @@
-'use client'
+"use client";
 import { useTracker } from "@/app/context/TrackerContext";
-import {AreaChart,Area,ResponsiveContainer} from 'recharts'
+import { AreaChart, Area, ResponsiveContainer } from "recharts";
 
 export default function Chart() {
-   const { data } = useTracker()
+  const { data } = useTracker();
   return (
-    <section className="container mx-auto my-4 p-4 shadow-xl rounded-2xl">
-      <div>Chart</div>
-      {/* <ResponsiveContainer width={1} height={400}>
-        <AreaChart data={data}>
-          <Area type="monotone" dataKey="qty" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
-      </ResponsiveContainer> */}
-    </section>
+    <ResponsiveContainer width={1} height={400}>
+      <AreaChart data={data}>
+        <Area type="monotone" dataKey="qty" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </ResponsiveContainer>
   );
 }
