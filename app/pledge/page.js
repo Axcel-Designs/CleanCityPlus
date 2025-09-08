90201e7cf98bcd507c7caef9e59073a93d7ff883
excel <axceldesigns@gmail.com>
2025-09-08 12:02:25 +0100

    pledge

--- app/pledge/page.js
+++ app/pledge/page.js
@@ -1,4 +1,6 @@
 "use client";
+
+import { Container, Row, Col } from "react-bootstrap";
 import PledgeForm from "@/components/pledge/PledgeForm";
 import PledgeList from "@/components/pledge/PledgeList";
 import { useEffect, useState } from "react";
@@ -15,32 +17,59 @@ export default function PledgePage() {
   }, []);
 
   function handlePledge(newPledge) {
-    setPledges([...pledges, newPledge]);
-    localStorage.setItem("formLocal", JSON.stringify([...pledges, newPledge]));
+    const updated = [...pledges, newPledge];
+    setPledges(updated);
+    localStorage.setItem("formLocal", JSON.stringify(updated));
   }
 
   return (
-    <main className="container mx-auto my-20">
-      <section className="text-center text-gray-800 mb-4 grid gap-2">
-        <p className="text-6xl text-green-700">Pledge</p>
-        <em>
-          Join our community of environmental champions. Make a commitment to
-          protect our planet and inspire others to take action.
-        </em>
-        <p className="text-sm text-yellow-700">
-          Pledge to reduce, reuse, and recycle. Your commitment helps build a
-          cleaner future.
-        </p>
-      </section>
-      <section className="my-4">
-        <div className="shadow-xl flex flex-col items-center text-gray-700 bg-blue-200 p-4 w-fit text-center rounded-2xl">
-          <FiUsers className="text-4xl text-blue-600" />
-          <p>Total Number of Pledges</p>
-          <p>{pledges.length}</p>
-        </div>
-      </section>
-      <PledgeForm onAddPledge={handlePledge} />
-      <PledgeList pledges={pledges} />
+    <main>
+      <Container className="my-20 bg-amber-400">
+        <section className="text-center text-gray-800 mb-4">
+          <Row className="gy-2">
+            <Col>
+              <p className="text-6xl text-green-700">Pledge</p>
+              <em>
+                Join our community of environmental champions. Make a commitment
+                to protect our planet and inspire others to take action.
+              </em>
+              <p className="text-sm text-yellow-700 mt-2">
+                Pledge to reduce, reuse, and recycle. Your commitment helps
+                build a cleaner future.
+              </p>
+            </Col>
+          </Row>
+        </section>
+
+        <section className="my-4">
+          <Row>
+            <Col xs="auto">
+              <div className="shadow-xl flex flex-col items-center text-gray-700 bg-blue-200 p-4 w-fit text-center rounded-2xl">
+                <FiUsers className="text-4xl text-blue-600" />
+                <p>Total Number of Pledges</p>
+                <p>{pledges.length}</p>
+              </div>
+            </Col>
+          </Row>
+        </section>
+
+        <section className="my-4">
+          <Row className="justify-content-center">
+            <Col lg md="auto">
+              <PledgeForm onAddPledge={handlePledge} />
+            </Col>
+          </Row>
+        </section>
+
+        {/* Pledge List */}
+        <section className="my-4">
+          <Row>
+            <Col>
+              <PledgeList pledges={pledges} />
+            </Col>
+          </Row>
+        </section>
+      </Container>
     </main>
   );
 }
